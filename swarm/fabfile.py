c2320f95cf92b7d6e6cd1520779ef247493b9f95
Lindemberg Barbosa <lindembergdepaulo@gmail.com>
2022-07-28 08:01:34 -0300

    feat: fabfile for swarm added

---
+++ swarm/fabfile.py
@@ -0,0 +1,126 @@
+from os import environ
+from fabric import Connection, task
+
+username = environ.get("USER")
+
+CLUSTER = {
+    "all": {
+        "address": ["node1", "node2", "node3"],
+    },
+    "main": {
+        "address": ["node1"],
+    },
+    "workers": {
+        "address": ["node2", "node3"],
+    },
+    "private_key": f"/home/{username}/.ssh/id_rsa",
+    "username": "ubuntu"
+}
+
+
+def con(hosts, user, private_key):
+    return Connection(
+        host=hosts,
+        user=user,
+        connect_kwargs={"key_filename": private_key},
+    )
+
+
+def _open_main_ports(c):
+    c.sudo(
+    """
+        sed -i "s/-A INPUT -j REJECT --reject-with icmp-host-prohibited//" /etc/iptables/rules.v4 && \
+        netfilter-persistent flush && \
+        netfilter-persistent start
+    """, hide=True, warn=True
+    )
+
+
+def _system_upgrade(c):
+    print("Installing requirements...")
+    c.sudo("apt-get update && sudo apt-get upgrade -y", hide=True)
+
+
+def _install_and_configure_docker(c):
+    print("Installing docker using convenience script...")
+    c.sudo("curl -sSL https://get.docker.com | sh -", hide=True)
+    c.sudo("usermod -aG docker $USER", hide=True)
+    c.run("id -g", hide=True)
+
+
+def _configure_main_swarm(c, main_address):
+    print("Initializing Docker Swarm cluster...")
+    c.run(f"docker swarm init --advertise-addr {main_address}", hide=True)
+
+
+def _get_join_worker_token(c):
+    print("Getting worker token...")
+    output = c.run("docker swarm join-token worker -q", hide=True)
+    worker_token = output.stdout.replace("\n", "")
+    return worker_token
+
+
+def _configure_workers_swarm(c, token, main_address):
+    print("Joining worker nodes to Docker Swarm...")
+    c.run(f"docker swarm join --token {token} {main_address}:2377")
+
+
+def _destroy_cluster(c):
+    c.run("docker swarm leave -f", hide=True, warn=True)
+
+
+def _deploy_application(c):
+    print("Deploying visualizer service...")
+    c.run(
+        """
+        docker service create \
+        --detach \
+        --name=viz \
+        --publish=8080:8080/tcp \
+        --constraint=node.role==manager \
+        --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+        alexellis2/visualizer-arm:latest
+    """,
+        hide=True,
+    )
+
+
+@task
+def swarm(c, upgrade="no", destroy="no"):
+    """
+    Deploy a Swarm cluster and create a visualizer service running on port 8080
+    Usage: fab swarm -u yes -d yes
+    -c, --upgrade - upgrade system
+    -d, --destroy - destroy Swarm cluster before create
+    """
+
+    all = CLUSTER["all"]
+    main = CLUSTER["main"]
+    workers = CLUSTER["workers"]
+    private_key = CLUSTER["private_key"]
+    username = CLUSTER["username"]
+
+    if destroy == "yes":
+        for hosts in all["address"]:
+            with con(hosts, username, private_key) as c:
+                _destroy_cluster(c)
+
+    if upgrade == "yes":
+        for hosts in all["address"]:
+            with con(hosts, username, private_key) as c:
+                _system_upgrade(c)
+                _install_and_configure_docker(c)
+
+    for hosts in main["address"]:
+        with con(hosts, username, private_key) as c:
+            _open_main_ports(c)
+            _configure_main_swarm(c, hosts)
+            token = _get_join_worker_token(c)
+            _deploy_application(c)
+
+    for hosts in workers["address"]:
+        with con(hosts, username, private_key) as c:
+            main_address = main["address"][0]
+            _configure_workers_swarm(c, token, main_address)
+
+    print("Swarm deployed 🎉")
